a4f836b4ddc676f2b9ba80ea8c36c84313d89431
Jose Carlos Bran <josecarlosbran16@gmail.com>
2022-10-07 10:57:49 -0600

    Observables parte 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -26,4 +26,9 @@ export class DishService {
     //return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
   }
 
+  getDishIds(): Observable<string[] | any> {
+
+    return of(DISHES.map(dish => dish.id));
+  }
+
 }
